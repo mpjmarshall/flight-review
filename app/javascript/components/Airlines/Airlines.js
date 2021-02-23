1b5e3d340e512a7737f76b2bcce1a19db7f0f615
mpjmarshall <mpj.marshall@gmail.com>
2021-02-22 20:21:39 -0600

    Added Airline component

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
+import Airline from "./Airline";
 
 const Airlines = () => {
-  return <div>This is a test to show Airlines#index</div>;
+  const [airlines, setAirlines] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/v1/airlines.json")
+      .then((resp) => {
+        setAirlines(resp.data.data);
+      })
+      .catch((resp) => console.log(resp));
+  }, [airlines.length]);
+
+  const grid = airlines.map((item) => {
+    return <Airline key={item.attributes.name} attributes={item.attributes} />;
+  });
+
+  return (
+    <Fragment>
+      <div className="home">
+        <div className="header">
+          <h1>OpenFlights</h1>
+          <div className="subheader">Honest reviews</div>
+        </div>
+        <div className="grid">{grid}</div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Airlines;
