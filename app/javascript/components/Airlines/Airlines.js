fb9c4476f2856f0589c2fbc0c52ae7b527761421
mpjmarshall <mpj.marshall@gmail.com>
2021-02-23 21:56:34 -0600

    Added syling to airlines conponent

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -3,6 +3,34 @@ import axios from "axios";
 import Airline from "./Airline";
 import styled from "styled-components";
 
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margtin-right: auto;
+`;
+
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+
+  h1 {
+    font-size: 42;
+  }
+`;
+
+const Subheader = styled.div`
+  font-weight: 300;
+  font-size: 26px;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+`;
+
 const Airlines = () => {
   const [airlines, setAirlines] = useState([]);
 
@@ -21,13 +49,13 @@ const Airlines = () => {
 
   return (
     <Fragment>
-      <div className="home">
-        <div className="header">
+      <Home>
+        <Header>
           <h1>OpenFlights</h1>
-          <div className="subheader">Honest reviews</div>
-        </div>
-        <div className="grid">{grid}</div>
-      </div>
+          <Subheader>Honest reviews</Subheader>
+        </Header>
+        <Grid>{grid}</Grid>
+      </Home>
     </Fragment>
   );
 };
