151808d05abd9dfa12996fb5ebddaa3adab5ca1e
mpjmarshall <mpj.marshall@gmail.com>
2021-02-24 22:50:35 -0600

    Added show page styling

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
 
-const Airline = () => {
-  return <div>This is a test to show Airline#show</div>;
+const Airline = (props) => {
+  const [airline, setAirline] = useState({});
+  const [review, setReview] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    const url = `/api/v1/airlines/${slug}`;
+    axios
+      .get(url)
+      .then((resp) => {
+        setAirline(resp.data);
+        setLoaded(true);
+      })
+      .catch((resp) => console.log(resp));
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <div className="column">
+        {loaded && (
+          <Header
+            attributes={airline.data.attributes}
+            reviews={airline.included}
+          />
+        )}
+        <div className="reviews"></div>
+      </div>
+      <div className="column">
+        <div className="review-form">[Review goes here]</div>
+      </div>
+    </div>
+  );
 };
 
 export default Airline;
---
+++ app/javascript/components/Airline/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = (props) => {
+  const { name, image_url, avg_score } = props.attributes;
+  const total = props.reviews.length;
+  return (
+    <div className="wrapper">
+      <h1>
+        <img src={image_url} height="50" width="50" alt={name} /> {name}
+      </h1>
+
+      <div>
+        <div className="totalReviews">{total} User Reviews</div>
+        <div className="starRating"></div>
+        <div className="totalOutOf">{avg_score} out of 5</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
