9057213b28b948a00e45ee466ca0afea6ae2f072
mpjmarshall <mpj.marshall@gmail.com>
2021-03-01 19:39:12 -0600

    Added styling to Header

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,6 +1,28 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Header from "./Header";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+const Column = styled.div`
+  background: #fff;
+  height: 100vh;
+  overflow: scroll;
+
+  &:last-child {
+    background: #000;
+  }
+`;
+
+const Main = styled.div`
+  padding-left: 50px;
+`;
 
 const Airline = (props) => {
   const [airline, setAirline] = useState({});
@@ -20,20 +42,22 @@ const Airline = (props) => {
   }, []);
 
   return (
-    <div className="wrapper">
-      <div className="column">
-        {loaded && (
-          <Header
-            attributes={airline.data.attributes}
-            reviews={airline.included}
-          />
-        )}
+    <Wrapper>
+      <Column>
+        <Main>
+          {loaded && (
+            <Header
+              attributes={airline.data.attributes}
+              reviews={airline.included}
+            />
+          )}
+        </Main>
         <div className="reviews"></div>
-      </div>
-      <div className="column">
+      </Column>
+      <Column>
         <div className="review-form">[Review goes here]</div>
-      </div>
-    </div>
+      </Column>
+    </Wrapper>
   );
 };
 
